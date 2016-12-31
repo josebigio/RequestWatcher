22e3cf016a1ad0d32a791ced3836af568ce6beb7
Jose Bigio <sirownsalot@gmail.com>
2016-12-31 13:48:44 -0600

    added a github retrofit

---
+++ app/src/main/java/com/josebigio/requestwatcher/Api.java
@@ -0,0 +1,28 @@
+package com.josebigio.requestwatcher;
+
+import java.util.List;
+
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+import rx.Observable;
+
+/**
+ * <h1>Api</h1>
+ */
+public interface Api {
+
+
+    // Request method and URL specified in the annotation
+    // Callback for the parsed response is the last parameter
+    @GET("/users/{username}")
+    Observable<User> getUser(@Path("username") String username);
+
+    @GET("/group/{id}/users")
+    Observable<List<User>> groupList(@Path("id") int groupId, @Query("sort") String sort);
+
+    @POST("/users/new")
+    Observable<User> createUser(@Body User user);
+}
--- app/src/main/java/com/josebigio/requestwatcher/MainActivity.java
+++ app/src/main/java/com/josebigio/requestwatcher/MainActivity.java
@@ -1,13 +1,62 @@
 package com.josebigio.requestwatcher;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.HttpException;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+import rx.Observable;
+import rx.Subscriber;
+import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String BASE_URL = "https://api.github.com";
+    private static Api apiService;
+    private static final String TAG = MainActivity.class.getCanonicalName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RxJavaCallAdapterFactory rxAdapter = RxJavaCallAdapterFactory.createWithScheduler(Schedulers.io());
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(rxAdapter)
+                .build();
+        apiService = retrofit.create(Api.class);
+        String username = "josebigio";
+        Observable<User> call = apiService.getUser(username);
+        Subscription subscription = call
+                .subscribeOn(Schedulers.io()) // optional if you do not wish to override the default behavior
+                .observeOn(AndroidSchedulers.mainThread()).subscribe(new Subscriber<User>() {
+                    @Override
+                    public void onCompleted() {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        // cast to retrofit.HttpException to get the response code
+                        Log.d(TAG,String.format("Error: %s",e));
+                        if (e instanceof HttpException) {
+                            HttpException response = (HttpException)e;
+                            int code = response.code();
+                        }
+                    }
+
+                    @Override
+                    public void onNext(User user) {
+                        Log.d(TAG,String.format("User: %s",user));
+                    }
+                });
+
     }
+
 }
